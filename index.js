90a0f0dcae5508430d9c99e7f748cc3056950ce5
karimkhaled <kkhaled@kibkw.com>
2023-10-08 20:31:19 +0300

    fix naming

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
   });
   
   app.get("/.well-known/assetlinks.json", (req, res) => {
-    let YOUR_PACKAGE_NAME=(process.env.YOUR_PACKAGE_NAME);
+    let package_name=(process.env.package_name);
     let sha256_cert_fingerprints=(process.env.sha256_cert_fingerprints);
 
     res.writeHead(200, {"Content-Type": "application/json"});
@@ -52,7 +52,7 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
         ],
         "target": {
           "namespace": "android_app",
-          "package_name": YOUR_PACKAGE_NAME,
+          "package_name": package_name,
           "sha256_cert_fingerprints": [
             sha256_cert_fingerprints,
           ],
@@ -61,20 +61,20 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
     ));
     console.log(applicationID)
 
-    res.end(YOUR_PACKAGE_NAME);
+    res.end(package_name);
   });
   
   app.get("*", (req, res, next) => {
     const title = 'My Amazing Application';
     const subtitle = 'Find out more about my app...';
     const image = 'https://dashboard.render.com/static/media/logo-redesign-02-word-dark.0811da26fe4b1f9a9b6c642d91bbcf73.svg';
-    let YOUR_PACKAGE_NAME=(process.env.YOUR_PACKAGE_NAME);
+    let package_name=(process.env.package_name);
     const templatePath = path.join(__dirname, 'index.html');
     var source = fs.readFileSync(templatePath, { encoding: 'utf-8' })
         .replace('{{title}}', title)
         .replace('{{subtitle}}', subtitle)
         .replace('{{image}}', image)
-        .replace('{{YOUR_APP_ID}}',YOUR_PACKAGE_NAME);
+        .replace('{{YOUR_APP_ID}}',package_name);
 
     return res.send(source);
   });
